d440f8805759308c03d7e33b678ad24bfc0be544
Alejandro Perez <42943714+Alejok0@users.noreply.github.com>
2023-04-22 17:03:10 -0600

    Ajuste del codigo en base a la plataforma

--- ESP32_MQTT_Coneccion_io.ino
+++ ESP32_MQTT_Coneccion_io.ino
@@ -1,124 +1,53 @@
-#include <WiFi.h>
-#include <PubSubClient.h>
-#include <WiFiClientSecure.h>
+//#define CAYENNE_DEBUG
+#define CAYENNE_PRINT Serial
+#include <CayenneMQTTESP32.h>
 
+// WiFi network info.
 const char* ssid = "red";
 const char* password = "contraseña-red";
 
+// Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
 const char* mqtt_server = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
-const int mqtt_port = 8883;
 const char* mqtt_username = "user";
 const char* mqtt_password = "pass";
+const char* client_id = "xxxxxxxxxxxxxxxxxxxxxx"
 
 // Definición de los canales a utilizar
-const char* CONTROL_LED_TOPIC = "control-led";
-const char* VALOR_ANALOGICO_TOPIC = "valor-analogico";
+const int CONTROL_LED_TOPIC = 0;
+const int VALOR_ANALOGICO_TOPIC = 1;
 
 const int ledPin = 26;
 const int inputPin = 36;
 
-static const char* root_ca PROGMEM = R"EOF(
------BEGIN CERTIFICATE-----
-MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
-TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
-cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
-WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
-ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
-MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
-h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
-0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
-A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
-T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
-B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
-B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
-KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
-OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
-jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
-qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
-rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
-HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
-hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
-ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
-3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
-NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
-ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
-TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
-jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
-oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
-4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
-mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
-emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
------END CERTIFICATE-----
-)EOF";
-
-WiFiClientSecure espClient;
-PubSubClient client(espClient);
-
-unsigned long lastMsg = 0;
-char msg[50];
-
-void setup_wifi() {
-  delay(10);
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-}
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Mensaje recibido en el canal: ");
-  Serial.println(topic);
-
-  if (strcmp(topic, CONTROL_LED_TOPIC) == 0) {
-    int estado = payload[0] - '0';
-    digitalWrite(ledPin, estado);
-    Serial.print("Estado del LED: ");
-    Serial.println(estado);
-  } else if (strcmp(topic, VALOR_ANALOGICO_TOPIC) == 0) {
-    int valor_analogico = analogRead(inputPin);
-    Serial.println("Valor analógico: "+String(valor_analogico));
-    client.publish(VALOR_ANALOGICO_TOPIC, String(valor_analogico).c_str(), true);
-    delay(2000);
-  }
-}
-
-void reconnect() {
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    if (client.connect("ESP32Client", mqtt_username, mqtt_password)) {
-      Serial.println("connected");
-      client.subscribe(CONTROL_LED_TOPIC);
-      client.subscribe(VALOR_ANALOGICO_TOPIC);
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      delay(5000);
-    }
-  }
-}
 
 void setup() {
   pinMode(ledPin, OUTPUT);
-  pinMode(inputPin, ANALOG);
+  pinMode(inputPin, ANALOG);  
   Serial.begin(9600);
-  setup_wifi();
-  espClient.setCACert(root_ca);
-  client.setServer(mqtt_server, mqtt_port);
-  client.setCallback(callback);
+  Cayenne.begin(mqtt_username, mqtt_password, client_id, ssid, password);
 }
 
 void loop() {
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
+  Cayenne.loop();
+}
+
+// Default function for sending sensor data at intervals to Cayenne.
+// You can also use functions for specific channels, e.g CAYENNE_OUT(1) for sending channel 1 data.
+CAYENNE_OUT_DEFAULT() {
+  int valor_analogico = analogRead(inputPin);
+  Serial.println(valor_analogico);
+  Cayenne.virtualWrite(VALOR_ANALOGICO_TOPIC, valor_analogico);
+}
+
+// Default function for processing actuator commands from the Cayenne Dashboard.
+// You can also use functions for specific channels, e.g CAYENNE_IN(1) for channel 1 commands.
+CAYENNE_IN(0) {  // Esta función se ejecuta cuando se recibe un mensaje en el canal 0
+  int value = getValue.asInt();
+  digitalWrite(ledPin, value);
+  CAYENNE_LOG("Channel %u, value %s", request.channel, getValue.asString());
+}
+
+CAYENNE_IN_DEFAULT() {
+  CAYENNE_LOG("Default: Channel %u, value %s", request.channel, getValue.asString());
+  //Process message here. If there is an error set an error message using getValue.setError(), e.g getValue.setError("Error message");
 }
